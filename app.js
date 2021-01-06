30a65edf9cc8e591a36645d439c771abba733fe5
Devon Wolfkiel <devonwolf@tutanota.com>
2021-01-06 15:46:03 -0800

    Add JavaScript event listeners and style manipulation

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 // import functions and grab DOM elements
+const welcomeElement = document.getElementById('welcome');
+const changeButton = document.getElementById('change');
+
+const mainBody = document.getElementById('main');
 
 // initialize state
 
+
 // set event listeners to update state and DOM
+
+changeButton.addEventListener('click', () => {
+	welcomeElement.style.color = 'lightgreen';
+	welcomeElement.style.background = 'black';
+	welcomeElement.textContent = '### HACKED ###';
+	welcomeElement.style.padding = '5%';
+	welcomeElement.style.margin = '5%';
+	welcomeElement.style.border = 'solid 5px lightgreen';
+
+	mainBody.style.background = 'black';
+	mainBody.style.color = 'lightgreen';
+});
